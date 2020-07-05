b3ade68c27d3bc2659e65f1d87e8a28028cefc6b
Arthur Briganti Gini <arthurbriganti@hotmail.com>
2020-07-05 00:31:05 -0300

    Textos de entrada definido

--- prepara_txt.py
+++ prepara_txt.py
@@ -5,8 +5,10 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
 stop_words = set(stopwords.words('portuguese'))
-stop_words.update(['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}'])
-stop_words.update(['ter','ainda','diz','cada','porém','eu','vai','pra','deu','aqui','lá','sido','nessa','feita'])
+stop_words.update(['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}','-','_'])
+stop_words.update(['faz','to','for','tipo','a','mó','seis','há','que','cá','cês','tem','lá','to','dá','ve','ia','vez','uns','quê','ai','si','né','aí','não','é','tô','tá','ter','ainda','diz','cada','porém','eu','vai','pra','deu','aqui','lá','sido','nessa','feita'])
+stop_words.update(['o','ô','ó','eu','vou','ró','vao','vão','pá','rá','pô'])
+stop_words.update(['1','2','3','4','5','6','7','8','9',''])
 
 entrada = open('entrada.txt', 'r', encoding='utf-8')
 
